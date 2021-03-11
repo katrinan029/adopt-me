4a9dafd28984c743b28cfff50434b93aba3cbeaa
Katrina Nguyen <katrinan@usc.edu>
2021-03-10 16:16:27 -0800

    adding carousel

---
+++ src/Carousel.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: [],
+      active: 0,
+    };
+
+    this.handleIndexClick = this.handleIndexClick.bind(this);
+  }
+  // state = {loading: true}
+  // state = {
+  //   photos: [],
+  //   active: 0,
+  // };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick(event) {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  }
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img
+              key={photo}
+              onClick={this.handleIndexClick.bind(this)}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
